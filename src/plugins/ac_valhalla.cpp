8837e5b42e3531497f28726c0d3947ac59992d2f
Kaldaien <kaldaien@differentk.fyi>
2022-04-02 18:43:46 -0400

    Fix for AC Valhalla when using local injection

--- src/plugins/ac_valhalla.cpp
+++ src/plugins/ac_valhalla.cpp
@@ -27,20 +27,14 @@
 #define ACVD_VERSION_NUM L"0.0.1"
 #define ACVD_VERSION_STR L"Assassin's Creed: Valhalla v " ACVD_VERSION_NUM
 
-//static NtReadFile_pfn NtReadFile_Original = nullptr;
-
 #include <SpecialK/diagnostics/file.h>
 
 void
 SK_ACV_InitPlugin (void)
 {
-//SK_SetPluginName (ACVD_VERSION_STR);
-
-  auto intro_vids =
-    SK_GetDLLConfig ()->get_section (L"ACValhalla.PlugIn").
-                        get_value   (L"DisableIntroVideos");
-
-  if (SK_IsTrue (intro_vids.c_str ()))
-  {
-  }
+  // Pending Removal:
+  //
+  //   This plug-in was never finished and what little there was has been
+  //     scattered throughout other parts of the code since.
+  //
 }
