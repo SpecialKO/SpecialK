1608182a29e7807645358731548bc0e0d5c1f834
Kaldaien <kaldaien@differentk.fyi>
2022-08-16 19:22:20 -0400

    Strip username from SEH exception logging

--- src/diagnostics/debug_utils.cpp
+++ src/diagnostics/debug_utils.cpp
@@ -3508,10 +3508,12 @@ RaiseException_Detour (
           _ReturnAddress (), wszCallerName
         );
 
+        SK_StripUserNameFromPathW (wszCallerName);
+
         SK_LOG0 ( ( L"Exception Code: %x  - Flags: (%hs) -  Arg Count: %u   "
                     L"[ Calling Module:  %s ]", dwExceptionCode,
                         SK_ExceptionFlagsToStr (dwExceptionFlags),
-                          nNumberOfArguments,      wszCallerName),
+                          nNumberOfArguments,     wszCallerName,
                     L"SEH-Except"
         );
 
