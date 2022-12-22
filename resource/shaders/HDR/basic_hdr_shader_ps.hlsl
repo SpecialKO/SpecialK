c948aabffb91f070452587ecc4ffa22accfc612b
Kaldaien <kaldaien@differentk.fyi>
2022-12-22 02:47:34 -0500

    Replaced exposure visualization LUT with shader used by HDR+WCG Image Viewer

--- resource/shaders/HDR/basic_hdr_shader_ps.hlsl
+++ resource/shaders/HDR/basic_hdr_shader_ps.hlsl
@@ -619,7 +619,7 @@ float4 main (PS_INPUT input) : SV_TARGET
   {
     hdr_color =
       float4 (0.0, 0.0, 0.0, 1.0);
-        
+
     float2 uv = input.uv;
     
     if ( (uv.x > 0.45 && uv.x < 0.55) &&
@@ -663,6 +663,7 @@ float4 main (PS_INPUT input) : SV_TARGET
 
     _fSDRTarget = fUserTarget / 18.0;
 
+    if (visualFunc.x != VISUALIZE_EXPOSURE)
     if (fLuma * 80.0 <= _fSDRTarget)
     {
       return
@@ -685,39 +686,101 @@ float4 main (PS_INPUT input) : SV_TARGET
         hdr_color.rgb =
           Clamp_scRGB (hdr_color.rgb);
 
+      float4 result;
+
+#if 0
       // find brightest component
       float lum   = max  (hdr_color.r, max (hdr_color.g, hdr_color.b));
-	    float Scale = log2 (lum / ((_fSDRTarget / 80.0) * 0.18)) / 2.0f + 2.0f;
-
-	    Scale = min (Scale, 7.0);
-	    Scale = max (Scale, 0.0);
-
-	    const float3 Colors[] =
-	    {
-	    	{ 0.0f, 0.0f, 0.0f },
-	    	{ 0.0f, 0.0f, 1.0f },
-	    	{ 0.0f, 1.0f, 1.0f },
-	    	{ 0.0f, 1.0f, 0.0f },
-	    	{ 1.0f, 1.0f, 0.0f },
-	    	//{ 1.0f, 0.5f, 0.0f },
-	    	{ 1.0f, 0.0f, 0.0f },
-	    	{ 1.0f, 0.0f, 1.0f },
-	    	{ 1.0f, 1.0f, 1.0f },
-	    	{ 1.0f, 1.0f, 1.0f }
-	    };
-	    int index = int (Scale);
-
-
-	    float4 result;
+      float Scale = log2 (lum / ((_fSDRTarget / 80.0) * 0.18)) / 2.0f + 2.0f;
+      
+      Scale = min (Scale, 7.0);
+      Scale = max (Scale, 0.0);
+      
+      const float3 Colors[] =
+      {
+        { 0.0f, 0.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f },
+        { 0.0f, 1.0f, 1.0f },
+        { 0.0f, 1.0f, 0.0f },
+        { 1.0f, 1.0f, 0.0f },
+        //{ 1.0f, 0.5f, 0.0f },
+        { 1.0f, 0.2f, 0.0f },
+        { 1.0f, 0.0f, 0.0f },
+        { 1.0f, 0.0f, 1.0f },
+        { 1.0f, 1.0f, 1.0f }
+      };
+      int index = int (Scale);
 
       result.rgb =
         lerp ( Colors [index    ],
                Colors [index + 1], Scale - index) *
          min (          Luminance (hdr_color.rgb) * 2.0F,
                                             0.15F * hdrLuminance_MaxAvg / 80.0 );
-	    result.a = 1;
+#else
+// Taken from https://github.com/microsoft/Windows-universal-samples/blob/main/Samples/D2DAdvancedColorImages/cpp/D2DAdvancedColorImages/LuminanceHeatmapEffect.hlsl in order to match HDR + WCG Image Viewer's Heatmap
+// 
+// Define constants based on above behavior: 9 "stops" for a piecewise linear gradient in scRGB space.
+#define STOP0_NITS 0.00f
+#define STOP1_NITS 3.16f
+#define STOP2_NITS 10.0f
+#define STOP3_NITS 31.6f
+#define STOP4_NITS 100.f
+#define STOP5_NITS 316.f
+#define STOP6_NITS 1000.f
+#define STOP7_NITS 3160.f
+#define STOP8_NITS 10000.f
 
-	    return result;
+#define STOP0_COLOR float4(0.0f, 0.0f, 0.0f, 1.0f) // Black
+#define STOP1_COLOR float4(0.0f, 0.0f, 1.0f, 1.0f) // Blue
+#define STOP2_COLOR float4(0.0f, 1.0f, 1.0f, 1.0f) // Cyan
+#define STOP3_COLOR float4(0.0f, 1.0f, 0.0f, 1.0f) // Green
+#define STOP4_COLOR float4(1.0f, 1.0f, 0.0f, 1.0f) // Yellow
+#define STOP5_COLOR float4(1.0f, 0.2f, 0.0f, 1.0f) // Orange
+// Orange isn't a simple combination of primary colors but allows us to have 8 gradient segments,
+// which gives us cleaner definitions for the nits --> color mappings.
+#define STOP6_COLOR float4(1.0f, 0.0f, 0.0f, 1.0f) // Red
+#define STOP7_COLOR float4(1.0f, 0.0f, 1.0f, 1.0f) // Magenta
+#define STOP8_COLOR float4(1.0f, 1.0f, 1.0f, 1.0f) // White
+
+      // 1: Calculate luminance in nits.
+      // Input is in scRGB. First convert to Y from CIEXYZ, then scale by whitepoint of 80 nits.
+      float nits = dot(float3(0.2126f, 0.7152f, 0.0722f), hdr_color.rgb) * 80.0f;
+
+      // 2: Determine which gradient segment will be used.
+      // Only one of useSegmentN will be 1 (true) for a given nits value.
+      float useSegment0 = sign(nits - STOP0_NITS) - sign(nits - STOP1_NITS);
+      float useSegment1 = sign(nits - STOP1_NITS) - sign(nits - STOP2_NITS);
+      float useSegment2 = sign(nits - STOP2_NITS) - sign(nits - STOP3_NITS);
+      float useSegment3 = sign(nits - STOP3_NITS) - sign(nits - STOP4_NITS);
+      float useSegment4 = sign(nits - STOP4_NITS) - sign(nits - STOP5_NITS);
+      float useSegment5 = sign(nits - STOP5_NITS) - sign(nits - STOP6_NITS);
+      float useSegment6 = sign(nits - STOP6_NITS) - sign(nits - STOP7_NITS);
+      float useSegment7 = sign(nits - STOP7_NITS) - sign(nits - STOP8_NITS);
+
+      // 3: Calculate the interpolated color.
+      float lerpSegment0 = (nits - STOP0_NITS) / (STOP1_NITS - STOP0_NITS);
+      float lerpSegment1 = (nits - STOP1_NITS) / (STOP2_NITS - STOP1_NITS);
+      float lerpSegment2 = (nits - STOP2_NITS) / (STOP3_NITS - STOP2_NITS);
+      float lerpSegment3 = (nits - STOP3_NITS) / (STOP4_NITS - STOP3_NITS);
+      float lerpSegment4 = (nits - STOP4_NITS) / (STOP5_NITS - STOP4_NITS);
+      float lerpSegment5 = (nits - STOP5_NITS) / (STOP6_NITS - STOP5_NITS);
+      float lerpSegment6 = (nits - STOP6_NITS) / (STOP7_NITS - STOP6_NITS);
+      float lerpSegment7 = (nits - STOP7_NITS) / (STOP8_NITS - STOP7_NITS);
+
+      //  Only the "active" gradient segment contributes to the output color.
+      result =
+          lerp(STOP0_COLOR, STOP1_COLOR, lerpSegment0) * useSegment0 +
+          lerp(STOP1_COLOR, STOP2_COLOR, lerpSegment1) * useSegment1 +
+          lerp(STOP2_COLOR, STOP3_COLOR, lerpSegment2) * useSegment2 +
+          lerp(STOP3_COLOR, STOP4_COLOR, lerpSegment3) * useSegment3 +
+          lerp(STOP4_COLOR, STOP5_COLOR, lerpSegment4) * useSegment4 +
+          lerp(STOP5_COLOR, STOP6_COLOR, lerpSegment5) * useSegment5 +
+          lerp(STOP6_COLOR, STOP7_COLOR, lerpSegment6) * useSegment6 +
+          lerp(STOP7_COLOR, STOP8_COLOR, lerpSegment7) * useSegment7;
+#endif
+      result.a = 1;
+      
+      return result;
     }
 
     if (((fLuma - (_fSDRTarget / 80.0)) / fDivisor) >= 1.0f)
