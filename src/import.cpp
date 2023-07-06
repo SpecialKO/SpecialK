b37e62bacdff71fcf0d4d2ed317db68e8dc02b3e
Kaldaien <kaldaien@differentk.fyi>
2023-07-05 22:56:53 -0400

    Bypass ReShade 5.x's ReShade.ini file check when loading plug-ins

--- src/import.cpp
+++ src/import.cpp
@@ -128,6 +128,13 @@ SK_Import_GetShimmedLibrary (HMODULE hModShim, HMODULE& hModReal)
 void
 SK_LoadImportModule (import_s& import)
 {
+  // Allow ReShade 5.2+ to be loaded globally, and rebase its config
+  SK_RunOnce (
+  {
+    SetEnvironmentVariableW (L"RESHADE_BASE_PATH_OVERRIDE",    SK_GetConfigPath ());
+    SetEnvironmentVariableW (L"RESHADE_DISABLE_LOADING_CHECK", L"1");
+  });
+
   if (config.system.central_repository)
   {
     wchar_t      wszProfilePlugIn [MAX_PATH + 2] = { };
