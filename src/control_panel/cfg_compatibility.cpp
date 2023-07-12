ef59616c3775f3c043553b29bcfb09396946b143
Kaldaien <kaldaien@differentk.fyi>
2023-07-12 13:10:14 -0400

    Add option to turn off D3DKMT PerfData to the Compatibility / Hardware Monitoring section of the Control Panel

--- src/control_panel/cfg_compatibility.cpp
+++ src/control_panel/cfg_compatibility.cpp
@@ -243,6 +243,11 @@ SK::ControlPanel::Compatibility::Draw (void)
       if (ImGui::IsItemHovered ())
         ImGui::SetTooltip ("AMD's hardware monitoring API, needed for the GPU stats on the OSD. Turn off only if your driver is buggy.");
       ImGui::TreePop  ();
+
+      ImGui::SameLine ();
+      ImGui::Checkbox ("D3DKMT",   &config.apis.D3DKMT.enable_perfdata);
+      if (ImGui::IsItemHovered ())
+        ImGui::SetTooltip ("Microsofts's hardware monitoring API, needed for the GPU stats on the OSD to supplement missing data from NvAPI or ADL. On some drivers, this may cause performance issues.");
     }
 
     ImGui::PushStyleColor (ImGuiCol_Header,        ImVec4 (0.90f, 0.40f, 0.40f, 0.45f));
