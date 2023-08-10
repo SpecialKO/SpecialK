d1dfcbf74eaeffac6bb8fece870679db8e445b73
Kaldaien <kaldaien@differentk.fyi>
2023-08-09 20:34:28 -0400

    Fixed missing headers for 32-bit

--- src/render/render_backend.cpp
+++ src/render/render_backend.cpp
@@ -29,7 +29,9 @@
 #include <SpecialK/render/dxgi/dxgi_backend.h>
 #include <SpecialK/render/d3d9/d3d9_backend.h>
 #include <SpecialK/render/d3d9/d3d9_texmgr.h>
+#include <SpecialK/render/d3d8/d3d8_backend.h>
 #include <SpecialK/render/gl/opengl_backend.h>
+#include <SpecialK/render/ddraw/ddraw_backend.h>
 #include <SpecialK/render/dxgi/dxgi_hdr.h>
 
 #include <SpecialK/nvapi.h>
