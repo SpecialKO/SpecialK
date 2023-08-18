11ca49bb57ccc7ab1353f6f5f7dcbcab7b94e745
Kaldaien <kaldaien@differentk.fyi>
2023-08-17 22:53:38 -0400

    Increase color gamut on SK's HDR UI

--- resource/shaders/imgui_d3d11_ps.hlsl
+++ resource/shaders/imgui_d3d11_ps.hlsl
@@ -24,16 +24,19 @@ Texture2D texture0    : register (t0);
 float4 main (PS_INPUT input) : SV_Target
 {
   float4 out_col =
-    texture0.Sample (sampler0, input.uv);
+    texture0.Sample (sampler0, input.uv),
+        orig_col =
+         out_col;
 
   bool hdr10 = ( input.uv3.x < 0.0 );
 
   if (viewport.z > 0.f)
   {
-    if (input.uv2.x > 0.0f && input.uv2.y > 0.0f)
+    if ( input.uv2.x > 0.0f &&
+         input.uv2.y > 0.0f )
     {
       out_col.rgb =
-        pow (
+        PositivePow (
           RemoveSRGBCurve (out_col.rgb),
                 input.uv2.yyy
             ) * input.uv2.xxx;
@@ -45,7 +48,7 @@ float4 main (PS_INPUT input) : SV_Target
       out_col =
         float4 ( RemoveSRGBCurve (          input.col.rgb) *
                  RemoveSRGBCurve (            out_col.rgb),
-                             pow (saturate (  out_col.a) *
+                     PositivePow (saturate (  out_col.a) *
                                   saturate (input.col.a), 0.8)
                );
     }
@@ -57,14 +60,21 @@ float4 main (PS_INPUT input) : SV_Target
 
     hdr_scale -= hdr_offset;
 
-    return
+    float4 hdr_out =
       float4 (   ( hdr10 ?
         LinearToST2084 (
-          REC709toREC2020 ( saturate (out_col.rgb) ) * hdr_scale
+          REC709toREC2020 (              saturate (out_col.rgb) ) * hdr_scale
                        ) :
-              Clamp_scRGB ( saturate (out_col.rgb)   * hdr_scale )
+     Clamp_scRGB_StripNaN ( expandGamut (saturate (out_col.rgb)   * hdr_scale, 0.25) )
                  )                                   + hdr_offset,
-                            saturate (out_col.a  ) );
+                                         saturate (out_col.a  ) );
+        
+    hdr_out.r = (orig_col.r <= 0.00015 && orig_col.r >= -0.00015) ? 0.0f : hdr_out.r;
+    hdr_out.g = (orig_col.g <= 0.00015 && orig_col.g >= -0.00015) ? 0.0f : hdr_out.g;
+    hdr_out.b = (orig_col.b <= 0.00015 && orig_col.b >= -0.00015) ? 0.0f : hdr_out.b;
+    hdr_out.a = (orig_col.a <= 0.00015 && orig_col.a >= -0.00015) ? 0.0f : hdr_out.a;
+        
+    return hdr_out;
   }
 
   return
