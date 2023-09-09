0edb58a6cd32fa201643b82e5bb0014301f94cd9
Kaldaien <kaldaien@differentk.fyi>
2023-09-09 06:20:47 -0400

    Added support for extended remastering to non-Steam version of Starfield

--- src/plugins/bethesda.cpp
+++ src/plugins/bethesda.cpp
@@ -25,7 +25,7 @@
 static iSK_INI* game_ini       = nullptr;
 static iSK_INI* gameCustom_ini = nullptr;
 
-#if 1
+#ifdef _WIN64
 
 static sk::ParameterFloat* sf_1stFOV = nullptr;
 static sk::ParameterFloat* sf_3rdFOV = nullptr;
@@ -47,91 +47,89 @@ uintptr_t CalculateOffset(uintptr_t uAddr) {
     return pBaseAddr + uAddr - 0x140000000;
 }
 
-bool SK_SF_PlugInCfg() {
-    std::string  utf8VersionString = SK_WideCharToUTF8(SK_GetDLLVersionStr(SK_GetHostApp()));
+bool SK_SF_PlugInCfg (void)
+{
+  static std::string  utf8VersionString =
+    SK_WideCharToUTF8 (SK_GetDLLVersionStr (SK_GetHostApp ()));
 
-    if (ImGui::CollapsingHeader(utf8VersionString.data(), ImGuiTreeNodeFlags_DefaultOpen))
+  if (ImGui::CollapsingHeader(utf8VersionString.data(), ImGuiTreeNodeFlags_DefaultOpen))
+  {
+    ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.90f, 0.40f, 0.40f, 0.45f));
+    ImGui::PushStyleColor(ImGuiCol_HeaderHovered, ImVec4(0.90f, 0.45f, 0.45f, 0.80f));
+    ImGui::PushStyleColor(ImGuiCol_HeaderActive, ImVec4(0.87f, 0.53f, 0.53f, 0.80f));
+    ImGui::TreePush("");
+
+    if (ImGui::CollapsingHeader ("Render Quality", ImGuiTreeNodeFlags_DefaultOpen))
     {
-        ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.90f, 0.40f, 0.40f, 0.45f));
-        ImGui::PushStyleColor(ImGuiCol_HeaderHovered, ImVec4(0.90f, 0.45f, 0.45f, 0.80f));
-        ImGui::PushStyleColor(ImGuiCol_HeaderActive, ImVec4(0.87f, 0.53f, 0.53f, 0.80f));
-        ImGui::TreePush("");
+      bool changed = false;
 
-        if (ImGui::CollapsingHeader ("Render Quality", ImGuiTreeNodeFlags_DefaultOpen))
-        {
-          bool changed = false;
+      ImGui::TreePush ("");
+      changed |= ImGui::Checkbox ("Upgrade Base RTs to 16-bpc color",       &bRemasterBasicRTs);
 
-          ImGui::TreePush ("");
-          changed |= ImGui::Checkbox ("Upgrade Base RTs to 16-bpc color",       &bRemasterBasicRTs);
+      if (ImGui::IsItemHovered ())
+        ImGui::SetTooltip ("Eliminates banding on UI at the cost of (negligible) extra VRAM");
 
-          if (ImGui::IsItemHovered ())
-            ImGui::SetTooltip ("Eliminates banding on UI at the cost of (negligible) extra VRAM");
+      ImGui::SameLine ();
 
-          // Only works in the Steam version right now
-          if (GetModuleHandle (L"steam_api64.dll"))
-          {
-            ImGui::SameLine ();
+      changed |= ImGui::Checkbox ("Upgrade Most 8-bit RTs to 16-bpc color", &bRemasterExtendedRTs);
 
-            changed |= ImGui::Checkbox ("Upgrade Most 8-bit RTs to 16-bpc color", &bRemasterExtendedRTs);
+      if (ImGui::IsItemHovered ())
+      {
+        ImGui::SetTooltip ("May further reduce banding and improve HDR, but at high memory cost");
+      }
 
-            if (ImGui::IsItemHovered ())
-            {
-              ImGui::SetTooltip ("May further reduce banding and improve HDR, but at high memory cost");
-            }
-          }
+      ImGui::TreePop ();
 
-          ImGui::TreePop ();
+      static bool restart_needed = false;
 
-          static bool restart_needed = false;
+      if (changed)
+      {
+        restart_needed = true;
 
-          if (changed)
-          {
-            restart_needed = true;
+        __SK_SF_BasicRemastering->store    (bRemasterBasicRTs);
+        __SK_SF_ExtendedRemastering->store (bRemasterExtendedRTs);
 
-            __SK_SF_BasicRemastering->store    (bRemasterBasicRTs);
-            __SK_SF_ExtendedRemastering->store (bRemasterExtendedRTs);
+        extern iSK_INI *dll_ini;
+        dll_ini->write ();
+      }
 
-            extern iSK_INI *dll_ini;
-            dll_ini->write ();
-          }
-
-          if (restart_needed)
-          {
-            ImGui::PushStyleColor (ImGuiCol_Text, (ImVec4&&)ImColor::HSV (.3f, .8f, .9f));
-            ImGui::BulletText     ("Game Restart Required");
-            ImGui::PopStyleColor  ();
-          }
-        }
-
-        if (pf1stFOV != nullptr)
-        {
-          bool changed = false;
-
-          if (ImGui::CollapsingHeader("Field of View"))
-          {
-            ImGui::TreePush("");
-
-            changed |= ImGui::SliderFloat("1st Person FOV", pf1stFOV, 1, 120, "%.0f");
-            changed |= ImGui::SliderFloat("3rd Person FOV", pf3rdFOV, 1, 120, "%.0f");
-
-            ImGui::TreePop();
-          }
-
-          if (changed)
-          {
-            sf_1stFOV->store(*pf1stFOV);
-            sf_3rdFOV->store(*pf3rdFOV);
-
-            gameCustom_ini->write();
-          }
-        }
-
-        ImGui::Separator();
-        ImGui::PopStyleColor(3);
-        ImGui::TreePop();
+      if (restart_needed)
+      {
+        ImGui::PushStyleColor (ImGuiCol_Text, (ImVec4&&)ImColor::HSV (.3f, .8f, .9f));
+        ImGui::BulletText     ("Game Restart Required");
+        ImGui::PopStyleColor  ();
+      }
     }
 
-    return true;
+    if (pf1stFOV != nullptr)
+    {
+      bool changed = false;
+
+      if (ImGui::CollapsingHeader("Field of View"))
+      {
+        ImGui::TreePush("");
+
+        changed |= ImGui::SliderFloat("1st Person FOV", pf1stFOV, 1, 120, "%.0f");
+        changed |= ImGui::SliderFloat("3rd Person FOV", pf3rdFOV, 1, 120, "%.0f");
+
+        ImGui::TreePop();
+      }
+
+      if (changed)
+      {
+        sf_1stFOV->store(*pf1stFOV);
+        sf_3rdFOV->store(*pf3rdFOV);
+
+        gameCustom_ini->write();
+      }
+    }
+
+    ImGui::Separator();
+    ImGui::PopStyleColor(3);
+    ImGui::TreePop();
+  }
+
+  return true;
 }
 
 void SK_SEH_InitStarfieldRTs (void)
@@ -145,9 +143,9 @@ void SK_SEH_InitStarfieldRTs (void)
 
       SK_LOGs0 (L"Starfield ", L"Scanned Address 0: %p", scan);
 
-      auto offset                           = *reinterpret_cast < int32_t *>((uintptr_t)scan + 3);
-      uint32_t *imageSpaceBufferPtr         =  reinterpret_cast <uint32_t *>((uintptr_t)scan + 7 + offset);            // 5079A70
-      uint32_t *scaleformCompositeBufferPtr =  reinterpret_cast <uint32_t *>((uintptr_t)imageSpaceBufferPtr + 0x280);  // 5079CF0
+      const auto offset                      = *reinterpret_cast < int32_t *>((uintptr_t)scan + 3);
+      uint32_t  *imageSpaceBufferPtr         =  reinterpret_cast <uint32_t *>((uintptr_t)scan + 7 + offset);            // 5079A70
+      uint32_t  *scaleformCompositeBufferPtr =  reinterpret_cast <uint32_t *>((uintptr_t)imageSpaceBufferPtr + 0x280);  // 5079CF0
 
       *imageSpaceBufferPtr         = 77;
       *scaleformCompositeBufferPtr = 77;
@@ -155,6 +153,14 @@ void SK_SEH_InitStarfieldRTs (void)
 
     if (bRemasterExtendedRTs)
     {
+      void *scan =
+        SK_ScanAlignedEx ("\x4C\x8D\x15\x00\x00\x00\x00\xBE\x00\x00\x00\x00", 12, "\xFF\xFF\xFF\x00\x00\x00\x00\xFF\x00\x00\x00\x00");
+
+      SK_LOGs0 (L"Starfield ", L"Scanned Buffer Array: %p", scan);
+
+			const auto offset  = *reinterpret_cast < int32_t *>((uintptr_t)scan  + 3);
+			const auto address =  reinterpret_cast < uintptr_t>(           scan) + 7 + offset;
+
       enum class BS_DXGI_FORMAT
       {
         BS_DXGI_FORMAT_UNKNOWN0 = 0,
@@ -308,7 +314,7 @@ void SK_SEH_InitStarfieldRTs (void)
       };
 
       BufferDefinition **buffer_defs =
-        (BufferDefinition **)((uintptr_t)CalculateOffset (0x144718E40));
+        reinterpret_cast <BufferDefinition **>(address);  // 4718E40
 
       const char *buffers_to_remaster [] =
       {
@@ -319,8 +325,6 @@ void SK_SEH_InitStarfieldRTs (void)
         "ImageSpaceBufferB10G11R11",
         "ImageSpaceBufferE5B9G9R9",
         "GBuffer_Normal_EmissiveIntensity",
-
-        //"FrameBuffer",
         "SF_ColorBuffer",
 
         "TAA_idTech7HistoryColorTarget",
@@ -337,7 +341,9 @@ void SK_SEH_InitStarfieldRTs (void)
         "SobelOutput",
         "SpaceGlareBlur",
         "SeparableSSSBufferUV",
+
 //FSR2_RESAMPLED_LUMA_HISTORY' (113) using FP16
+
         "ThinGBuffer_Albedo",
         "ThinGBuffer_Optional",
         "ThinGBuffer_AlbedoArray",
@@ -421,51 +427,51 @@ HRESULT __stdcall CreateVolumeTextureFromFileInMemoryHookForD3D9(LPDIRECT3DDEVIC
 void
 SK_BGS_InitPlugin(void)
 {
-    SK_GAME_ID gameID = SK_GetCurrentGameID();
-
+  SK_GAME_ID gameID = SK_GetCurrentGameID();
+  
 #ifdef _WIN64
-    if (gameID == SK_GAME_ID::Starfield)
+  if (gameID == SK_GAME_ID::Starfield)
+  {
+    __SK_SF_BasicRemastering =
+      _CreateConfigParameterBool ( L"Starfield.PlugIn",
+                                   L"BasicRTUpgrades", bRemasterBasicRTs,
+                                                        L"Promote Simple RTs to FP16" );
+  
+    __SK_SF_ExtendedRemastering =
+      _CreateConfigParameterBool ( L"Starfield.PlugIn",
+                                   L"ExtendedRTUpgrades", bRemasterExtendedRTs,
+                                                          L"Promote All 8-bit RTs to FP16" );
+  
+    if (bRemasterBasicRTs || bRemasterExtendedRTs)
     {
-      __SK_SF_BasicRemastering =
-        _CreateConfigParameterBool ( L"Starfield.PlugIn",
-                                     L"BasicRTUpgrades", bRemasterBasicRTs,
-                                                          L"Promote Simple RTs to FP16" );
-
-      __SK_SF_ExtendedRemastering =
-        _CreateConfigParameterBool ( L"Starfield.PlugIn",
-                                     L"ExtendedRTUpgrades", bRemasterExtendedRTs,
-                                                            L"Promote All 8-bit RTs to FP16" );
-
-      if (bRemasterBasicRTs || bRemasterExtendedRTs)
-      {
-        SK_SEH_InitStarfieldRTs ();
-      }
-
-      if (SK_GetModuleHandle (L"steam_api64.dll"))
-      {
-        if (game_ini == nullptr) {
-            game_ini = SK_CreateINI(LR"(.\Starfield.ini)");
-        }
-
-        if (gameCustom_ini == nullptr) {
-            gameCustom_ini = SK_CreateINI((SK_GetDocumentsDir() + LR"(\My Games\Starfield\StarfieldCustom.ini)").c_str());
-        }
-
-        game_ini->set_encoding       (iSK_INI::INI_UTF8NOBOM);
-        gameCustom_ini->set_encoding (iSK_INI::INI_UTF8NOBOM);
-
-        sf_1stFOV = dynamic_cast <sk::ParameterFloat*> (g_ParameterFactory->create_parameter <float>(L"First Person FOV"));
-        sf_3rdFOV = dynamic_cast <sk::ParameterFloat*> (g_ParameterFactory->create_parameter <float>(L"Third Person FOV"));
-        
-        sf_1stFOV->register_to_ini(gameCustom_ini, L"Camera", L"fFPWorldFOV");
-        sf_3rdFOV->register_to_ini(gameCustom_ini, L"Camera", L"fTPWorldFOV");
-
-        pf1stFOV = reinterpret_cast<float*>(CalculateOffset(0x14557B930) + 8);
-        pf3rdFOV = reinterpret_cast<float*>(CalculateOffset(0x14557B910) + 8);
-      }
-
-      plugin_mgr->config_fns.emplace(SK_SF_PlugInCfg);
+      SK_SEH_InitStarfieldRTs ();
     }
+  
+    if (SK_GetModuleHandle (L"steam_api64.dll"))
+    {
+      if (game_ini == nullptr) {
+          game_ini = SK_CreateINI(LR"(.\Starfield.ini)");
+      }
+  
+      if (gameCustom_ini == nullptr) {
+          gameCustom_ini = SK_CreateINI((SK_GetDocumentsDir() + LR"(\My Games\Starfield\StarfieldCustom.ini)").c_str());
+      }
+  
+      game_ini->set_encoding       (iSK_INI::INI_UTF8NOBOM);
+      gameCustom_ini->set_encoding (iSK_INI::INI_UTF8NOBOM);
+  
+      sf_1stFOV = dynamic_cast <sk::ParameterFloat*> (g_ParameterFactory->create_parameter <float>(L"First Person FOV"));
+      sf_3rdFOV = dynamic_cast <sk::ParameterFloat*> (g_ParameterFactory->create_parameter <float>(L"Third Person FOV"));
+      
+      sf_1stFOV->register_to_ini(gameCustom_ini, L"Camera", L"fFPWorldFOV");
+      sf_3rdFOV->register_to_ini(gameCustom_ini, L"Camera", L"fTPWorldFOV");
+  
+      pf1stFOV = reinterpret_cast<float*>(CalculateOffset(0x14557B930) + 8);
+      pf3rdFOV = reinterpret_cast<float*>(CalculateOffset(0x14557B910) + 8);
+    }
+  
+    plugin_mgr->config_fns.emplace(SK_SF_PlugInCfg);
+  }
 #else
 
     // Forces D3DPOOL_DEFAULT pool in order to allow texture debugging and caching
