4945c8af2c81fae7c5262c098c306c25f6689215
Kaldaien <kaldaien@differentk.fyi>
2023-09-30 11:38:18 -0400

    Add various device type GUIDs to Device Change Notification logic

--- src/window.cpp
+++ src/window.cpp
@@ -5057,6 +5057,13 @@ bool __ignore = false;
 #define WM_NCUAHDRAWFRAME   (0x00AF)
 #endif
 
+#include <initguid.h>
+#include <Ntddkbd.h>
+#include <Ntddmou.h>
+#include <Ntddvdeo.h>
+#include <Hidclass.h>
+#include <Bthdef.h>
+
 __declspec (noinline)
 LRESULT
 CALLBACK
@@ -5417,16 +5424,40 @@ SK_DetourWindowProc ( _In_  HWND   hWnd,
             const auto pDevW =
               (DEV_BROADCAST_DEVICEINTERFACE_W *)pDevHdr;
 
-            static constexpr GUID GUID_DEVINTERFACE_HID     =
-              { 0x4D1E55B2L, 0xF16F, 0x11CF, { 0x88, 0xCB, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 } };
-
             static constexpr GUID GUID_XUSB_INTERFACE_CLASS =
               { 0xEC87F1E3L, 0xC13B, 0x4100, { 0xB5, 0xF7, 0x8B, 0x84, 0xD5, 0x42, 0x60, 0xCB } };
 
-            if (IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_HID) ||
-                IsEqualGUID (pDevW->dbcc_classguid, GUID_XUSB_INTERFACE_CLASS))
+            // Input Devices
+            if (IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_HID)      ||
+                IsEqualGUID (pDevW->dbcc_classguid, GUID_XUSB_INTERFACE_CLASS)  ||
+                IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_KEYBOARD) ||
+                IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_MOUSE)    ||
+                IsEqualGUID (pDevW->dbcc_classguid, GUID_BTHPORT_DEVICE_INTERFACE))
             {
               bIgnore = false;
+            }
+
+            // Audio Devices
+            else if (IsEqualGUID (pDevW->dbcc_classguid, KSCATEGORY_CAPTURE) ||
+                     IsEqualGUID (pDevW->dbcc_classguid, KSCATEGORY_RENDER)  ||
+                     IsEqualGUID (pDevW->dbcc_classguid, KSCATEGORY_MIXER))
+            {
+              bIgnore = false;
+            }
+
+            // Display Hardware
+            else if (IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_MONITOR) ||
+                     IsEqualGUID (pDevW->dbcc_classguid, GUID_DEVINTERFACE_DISPLAY_ADAPTER))
+            {
+              bIgnore = false;
+            }
+
+            else
+            {
+              wchar_t                                 wszGUID [41] = { };
+              StringFromGUID2 (pDevW->dbcc_classguid, wszGUID, 40);
+
+              SK_LOGi0 (L" || Device=%ws", wszGUID);
             }
           }
         } break;
