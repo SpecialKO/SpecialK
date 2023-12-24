604989f24b231cd96761a74fb586dd67bb9146e1
Kaldaien <kaldaien2+github@gmail.com>
2023-12-24 08:38:43 -0500

    Pin nvofapi{64}.dll so that DLSS-G doesn't keep loading and unloading it when resizing SwapChains

--- src/diagnostics/load_library.cpp
+++ src/diagnostics/load_library.cpp
@@ -214,6 +214,7 @@ SK_LoadLibrary_IsPinnable (const _T* pStr)
 
     SK_TEXT ("nvapi"), SK_TEXT ("NvCameraAllowlisting"),
 
+    SK_TEXT ("nvofapi"), // DLSS-G spam reloads this when changing resolution
 
     SK_TEXT ("kbd"), // Keyboard Layouts take > ~20 ms to load, leave 'em loaded
 
