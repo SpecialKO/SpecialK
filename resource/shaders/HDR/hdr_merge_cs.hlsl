ada0665ac54eb6cfe8475a87c4261625bb957ad7
Kaldaien <kaldaien2+github@gmail.com>
2023-12-30 17:44:55 -0500

    Reduced flicker when using adaptive tonemapping

--- resource/shaders/HDR/hdr_merge_cs.hlsl
+++ resource/shaders/HDR/hdr_merge_cs.hlsl
@@ -45,20 +45,36 @@ LuminanceMerge ( uint3 globalIdx : SV_DispatchThreadID,
   if ( samples > 0 &&
          accum > 0 )
   {
-    float old0 = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 1, HISTOGRAM_WORKGROUP_SIZE_Y)];
-    float old1 = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 2, HISTOGRAM_WORKGROUP_SIZE_Y)];
-    float old2 = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 3, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float new_lum =
+      (accum / samples);
 
-    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X, HISTOGRAM_WORKGROUP_SIZE_Y)] =
-      ( accum / samples );
+    float old0  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  0, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old1  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  1, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old2  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  2, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old3  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  3, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old4  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  4, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old5  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  5, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old6  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  6, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old7  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  7, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old8  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  8, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old9  = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  9, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old10 = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 10, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    float old11 = texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 11, HISTOGRAM_WORKGROUP_SIZE_Y)];
 
-    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 2, HISTOGRAM_WORKGROUP_SIZE_Y)] =
-    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 1, HISTOGRAM_WORKGROUP_SIZE_Y)];
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X    , HISTOGRAM_WORKGROUP_SIZE_Y)] =
+      ( old0 + old1  + old2 + old3  + old4  + old5  + old6  + old7  +
+        old8 + old9 + old10 + old11 + new_lum ) / 13.0;
 
-    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 1, HISTOGRAM_WORKGROUP_SIZE_Y)] =
-      ( ( accum / samples ) * 8.0f + old0 * 4.0f + old1 * 2.0f + old2 * 1.0f) / 15.0;
-
-    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 3, HISTOGRAM_WORKGROUP_SIZE_Y)] =
-      old1;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  1, HISTOGRAM_WORKGROUP_SIZE_Y)] = old0;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  2, HISTOGRAM_WORKGROUP_SIZE_Y)] = old1;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  3, HISTOGRAM_WORKGROUP_SIZE_Y)] = old2;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  4, HISTOGRAM_WORKGROUP_SIZE_Y)] = old3;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  5, HISTOGRAM_WORKGROUP_SIZE_Y)] = old4;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  6, HISTOGRAM_WORKGROUP_SIZE_Y)] = old5;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  7, HISTOGRAM_WORKGROUP_SIZE_Y)] = old6;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  8, HISTOGRAM_WORKGROUP_SIZE_Y)] = old7;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X +  9, HISTOGRAM_WORKGROUP_SIZE_Y)] = old8;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 10, HISTOGRAM_WORKGROUP_SIZE_Y)] = old9;
+    texLuminance [uint2 (HISTOGRAM_WORKGROUP_SIZE_X + 11, HISTOGRAM_WORKGROUP_SIZE_Y)] = old10;
   }
 }
