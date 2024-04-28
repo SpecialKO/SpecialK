c9ba00b17e5bdcd172d85c4d73f3e7e67766c999
Kaldaien <kaldaien2+github@gmail.com>
2024-04-28 13:02:55 -0400

    Set 33% Color Boost in default HDR profiles

--- src/widgets/hdr.cpp
+++ src/widgets/hdr.cpp
@@ -141,8 +141,8 @@ struct SK_HDR_PQBoostParams
   float PQBoost2;
   float PQBoost3;
   float EstimatedMaxCLLScale;
-} SK_HDR_PQBoost_v0 = {  false, 30.0f, 11.5f, 1.500f, 1.0f,  570.0f },
-  SK_HDR_PQBoost_v1 = {  false,  1.0f,  0.1f, 1.273f, 0.5f,  267.0f };
+} SK_HDR_PQBoost_v0 = { 0.333f, 30.0f, 11.5f, 1.500f, 1.0f,  570.0f },
+  SK_HDR_PQBoost_v1 = { 0.333f,  1.0f,  0.1f, 1.273f, 0.5f,  267.0f };
 
 float __SK_HDR_ColorBoost = SK_HDR_PQBoost_v1.ColorBoost;
 float __SK_HDR_PQBoost0   = SK_HDR_PQBoost_v1.PQBoost0;
