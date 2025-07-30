230285241378364a326e121d5fa6cd980d4d1bbd
AnubyteCode <167210503+AnubyteCode@users.noreply.github.com>
2025-07-30 03:53:30 -0600

    Update cfg_steam.cpp
    Signed-off-by: AnubyteCode <167210503+AnubyteCode@users.noreply.github.com>

--- src/control_panel/cfg_steam.cpp
+++ src/control_panel/cfg_steam.cpp
@@ -45,7 +45,7 @@ struct denuvo_file_s
   SYSTEMTIME   st_local;
 };
 
-extern SK_LazyGlobal <std::vector <denuvo_file_s>> denuvo_files;
+SK_LazyGlobal<concurrency::concurrent_vector<denuvo_file_s>> denuvo_files;
 
 bool
 SK::ControlPanel::Steam::Draw (void)
