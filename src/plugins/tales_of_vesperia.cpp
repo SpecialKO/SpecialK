ff5f6ed1a64f9f1986032ae7690183bdad405f70
Lucas Cota <lucasbrunocota@live.com>
2025-11-28 23:52:43 -0300

    Bump version

--- src/plugins/tales_of_vesperia.cpp
+++ src/plugins/tales_of_vesperia.cpp
@@ -26,7 +26,7 @@
 #include <SpecialK/render/d3d11/d3d11_core.h>
 
 
-#define TVFIX_VERSION_NUM L"0.5.2.5 "
+#define TVFIX_VERSION_NUM L"0.5.2.6 "
 #define TVFIX_VERSION_STR LR"(Tales of Vesperia "Fix" v )" TVFIX_VERSION_NUM
 
 #pragma warning(push)
