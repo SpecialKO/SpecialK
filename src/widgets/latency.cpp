fce396741951bc41e6c232fef6a9d299490dd90c
Kaldaien <kaldaien2+github@gmail.com>
2026-04-16 22:23:15 -0400

    Rename "Composite" to "Scanout" in Latency Widget

--- src/widgets/latency.cpp
+++ src/widgets/latency.cpp
@@ -554,7 +554,7 @@ SK_ImGui_DrawGraph_Latency (bool predraw)
 
        ImPlot::PlotLine ("Simulation",    &history.sample_age [head], &history.simulation    [head], elements);
        ImPlot::PlotLine ("Render Submit", &history.sample_age [head], &history.render_submit [head], elements);
-       ImPlot::PlotLine ("Composite",     &history.sample_age [head], &history.frame_total   [head], elements);
+       ImPlot::PlotLine ("Scanout",       &history.sample_age [head], &history.frame_total   [head], elements);
        ImPlot::PlotLine ("GPU Busy",      &history.sample_age [head], &history.gpu_active    [head], elements);
        ImPlot::PlotLine ("CPU Busy",      &history.sample_age [head], &history.gpu_start     [head], elements);
      };
