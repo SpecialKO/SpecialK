188a3c0d21b21f5f18a1ea06966ff4b2590921f5
Kaldaien <kaldaien2+github@gmail.com>
2026-06-28 14:33:51 -0400

    Use ShowWindowAsync during DLL detach instead of ShowWindow

--- src/SpecialK.cpp
+++ src/SpecialK.cpp
@@ -1725,7 +1725,7 @@ SK_Detach (DLL_ROLE role)
   // Hide game while exiting to make it appear snappier.
   if (game_window.hWnd != 0)
   {
-    ShowWindow (game_window.hWnd, SW_HIDE);
+    SK_ShowWindowAsync (game_window.hWnd, SW_HIDE);
   }
 
   ULONG local_refs =
